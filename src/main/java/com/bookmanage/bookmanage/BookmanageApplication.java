f879b630ba667820b4e2419a21955da675376464
guanghuixu <guanghuixu@yangqianguan.com>
2020-02-15 20:18:27 +0800

    支持jar包

--- src/main/java/com/bookmanage/bookmanage/BookmanageApplication.java
+++ src/main/java/com/bookmanage/bookmanage/BookmanageApplication.java
@@ -3,10 +3,11 @@ package com.bookmanage.bookmanage;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.bookmanage.bookmanage.model"})
-public class BookmanageApplication {
+public class BookmanageApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BookmanageApplication.class, args);
